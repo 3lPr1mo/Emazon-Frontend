b494a8fb549b2ec6a9f57b54a09228d0c46b5533
3lPr1mo <erickenriquechaparro@gmail.com>
2024-10-04 21:49:56 -0500

    refactor(atoms): declare primary input component

--- src/app/components/shared/atoms/atoms.module.ts
+++ src/app/components/shared/atoms/atoms.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimaryButtonComponent } from './primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './secondary-button/secondary-button.component';
+import { PrimaryInputComponent } from './primary-input/primary-input.component';
 
 
 
 @NgModule({
   declarations: [
     PrimaryButtonComponent,
-    SecondaryButtonComponent
+    SecondaryButtonComponent,
+    PrimaryInputComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     PrimaryButtonComponent,
-    SecondaryButtonComponent
+    SecondaryButtonComponent,
+    PrimaryInputComponent
   ]
 })
 export class AtomsModule { }
