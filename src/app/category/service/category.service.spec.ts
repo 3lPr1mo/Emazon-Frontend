bc33b739f6df798ee27b30b9339027aa9d962f87
3lPr1mo <erickenriquechaparro@gmail.com>
2024-10-17 13:15:40 -0500

    test(category service): tests pass coverage 100%

--- src/app/category/service/category.service.spec.ts
+++ src/app/category/service/category.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { CategoryService } from './category.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {CreateCategoryRequest} from "../dtos/request/CreateCategory.request";
+import { ContentPage } from 'src/app/common/dto/response/paged.response';
 
 describe('CategoryService', () => {
   let service: CategoryService;
@@ -36,6 +37,32 @@ describe('CategoryService', () => {
     expect(req.request.method).toBe('POST');
     expect(req.request.url).toBe('http://localhost:8080/category');
     expect(req.request.body).toBe(mockCategory);
+  });
+
+  test('should return ContentPage with correct URL', () => {
+    const mockContentPage : ContentPage = {
+      totalPage:     0,
+      totalElements: 1,
+      pageNumber:    0,
+      pageSize:      3,
+      first:         true,
+      last:          true,
+      content:       [{id: 1, name: 'a', description: 'aa'}]
+    }
+
+    service.getAllCategories(0,3,true,'name').subscribe(
+      data => {
+        expect(data).toEqual(mockContentPage);
+      }
+    );
+
+    const req = httpMock.expectOne(
+      'http://localhost:8080/category?page=0&size=3&isAsc=true&sortBy=name'
+    );
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockContentPage);
+
   });
 
 });
